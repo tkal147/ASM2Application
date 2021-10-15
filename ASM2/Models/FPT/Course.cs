7352a5036e11754d69ab8306dd8acefe79a841d5
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2021-10-15 14:07:38 +0700

    comment list trainer and list trainee
    comment list trainer and list trainee

--- ASM2/Models/FPT/Course.cs
+++ ASM2/Models/FPT/Course.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
 namespace ASM2.Models.FPT
 {
     public class Course
@@ -14,9 +15,9 @@ namespace ASM2.Models.FPT
         [Required(ErrorMessage = "Please enter  your Descrpition")]
         public string Descrpitipon { get; set; }
         
-        public List<Trainee> Trainee { get; set; }
+        //public List<Trainee> Trainee { get; set; }
         
-        public List<Trainer> Trainer { get; set; }
+        //public List<Trainer> Trainer { get; set; }
         public int CatId { get; set; }
         //public Categories Category { get; set; }
     }
