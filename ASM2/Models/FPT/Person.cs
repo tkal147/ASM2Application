22eb9b9d18313c391780c3e3ded66ad24517100d
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2021-10-15 13:30:52 +0700

    gộp bảng
    gộp bảng

---
+++ ASM2/Models/FPT/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASM2.Models.FPT
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+        //staff, admin, trainer, trainee
+        public string Role { get; set; }
+        //staff, admin
+        public string Contact { get; set; }
+        //trainee
+        public string Education { get; set; }
+        ///trainee
+        public string DOB { get; set; }
+        //trainee
+        public string Age { get; set; }
+        //trainee
+        public string Department { get; set; }
+        //trainee
+        public string TOEIC { get; set; }
+        //trainee
+        public string Location { get; set; }
+        //trainer
+        public string Telephone { get; set; }
+        //trainer
+        public string Email { get; set; }
+        //trainer
+        public string Type { get; set; }
+        //trainer
+        public string WorkingPlace { get; set; }
+        //trainer, trainee
+        public List<Course> Courses { get; set; }
+        //all
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
