8ce5b8f7b04c30d8d7c3fdd9892e7d18077736be
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2021-10-15 13:31:57 +0700

    get set gộp bảng
    get set gộp bảng

--- ASM2/EF/FPTContext.cs
+++ ASM2/EF/FPTContext.cs
@@ -15,10 +15,9 @@ namespace ASM2.EF
         }
         public DbSet<Course> Course { get; set; }
         public DbSet<Categories> Categories { get; set; }
-        public DbSet<Trainer> Trainer { get; set; }
-        public DbSet<Trainee> Trainee { get; set; }
-        public DbSet<TrainingStaff> TrainingStaff { get; set; }
-        public DbSet<Admin> Admin { get; set; }
+
+        public DbSet<Person> Person { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder) // override DbContext's
         {
             modelBuilder.Entity<Course>()
@@ -27,34 +26,17 @@ namespace ASM2.EF
             modelBuilder.Entity<Course>()
                         .HasKey<int>(b => b.Id);  //setup PK
 
-            modelBuilder.Entity<Admin>()
-                        .ToTable("Admin"); //table name in db
 
-            modelBuilder.Entity<Admin>()
-                        .HasKey<int>(b => b.Id);  //setup PK
-
-            modelBuilder.Entity<TrainingStaff>()
-                        .ToTable("TrainingStaff"); //table name in db
-
-            modelBuilder.Entity<TrainingStaff>()
-                        .HasKey<int>(b => b.Id);  //setup PK
-
-            modelBuilder.Entity<Trainee>()
-                        .ToTable("Trainee"); //table name in db
-
-            modelBuilder.Entity<Trainee>()
-                        .HasKey<int>(b => b.Id);  //setup PK
-
-            modelBuilder.Entity<Trainer>()
-                        .ToTable("Trainer"); //table name in db
-
-            modelBuilder.Entity<Trainer>()
-                        .HasKey<int>(b => b.Id);  //setup PK
 
             modelBuilder.Entity<Categories>()
                         .ToTable("Categories"); //table name in db
 
             modelBuilder.Entity<Categories>()
+                        .HasKey<int>(b => b.Id);  //setup PK
+            modelBuilder.Entity<Person>()
+                       .ToTable("Person"); //table name in db
+
+            modelBuilder.Entity<Person>()
                         .HasKey<int>(b => b.Id);  //setup PK
         }
     }
